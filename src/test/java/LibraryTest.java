3ad55ffa94f7017af06e227da7bfba2430c6d592
Chris Beams <chris@beams.io>
2014-03-23 12:59:12 +0100

    Use assertThat instead of assertTrue
    http://forums.gradle.org/gradle/topics/improvement_ideas_for_the_build_init_plugin

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,12 +1,12 @@
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 public class LibraryTest {
 
     @Test
     public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        assertThat(new Library().someLibraryMethod(), is(true));
     }
 }
