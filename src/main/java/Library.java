b694a26d945638116ee51d30b9d8f38b5182ce18
Chris Beams <chris@beams.io>
2014-03-23 12:59:22 +0100

    Add required field, constructor and getter to Library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,5 +1,15 @@
 public class Library {
 
+    private final String name;
+
+    public Library(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
     public boolean someLibraryMethod() {
         return true;
     }
